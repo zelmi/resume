a85ae0ac27c85070d52bb5c994e2116654722219
zelmi <zehurelmi@gmail.com>
2021-03-13 11:19:06 -0500

    create blog h

---
+++ blog/blog_pages/blog_h/main.js
@@ -0,0 +1,19 @@
+$(function () {
+  $('a[href*=#]').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
+  });
+});
+
+new Sign().print(document.body)
+
+
+// hover div 
+var target = document.getElementById("trigger");
+
+function tapOrClick(event) {
+  return false;
+}
+
+target.addEventListener("touchstart", tapOrClick, false);
+
